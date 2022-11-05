bc0d82e47c654a65649d3be1a6cc2c1351be168a
Bryan Axl Coronado Cabrera <axlville5@gmail.com>
2022-11-05 10:38:42 -0600

    Squashed commit of the following:
    commit 3bb1ccc36f2aadf47aa8d69917a002a2b1520d6c
    Author: Edson Daniel Navarro Brizuela <39354713+Edson0710@users.noreply.github.com>
    Date:   Thu Nov 3 23:36:03 2022 -0600
    
        Edson (#2)
    
        * Diseño inicial index
    
        * Mejora en plantillas
    
    commit 983b523a64734076da2f20141759a2d45f569a64
    Author: Edson Daniel Navarro Brizuela <39354713+Edson0710@users.noreply.github.com>
    Date:   Thu Oct 20 19:07:06 2022 -0500
    
        Diseño inicial index (#1)

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.main')
+@section('title', 'Contact')
+@section('content')
+<!-- Page Header Start -->
+<div class="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
+    <div class="container text-center py-5">
+        <h1 class="display-3 text-white text-uppercase mb-3 animated slideInDown">Contact</h1>
+        <nav aria-label="breadcrumb animated slideInDown">
+            <ol class="breadcrumb justify-content-center text-uppercase mb-0">
+                <li class="breadcrumb-item"><a class="text-white" href="#">Home</a></li>
+                <li class="breadcrumb-item"><a class="text-white" href="#">Pages</a></li>
+                <li class="breadcrumb-item text-primary active" aria-current="page">Contact</li>
+            </ol>
+        </nav>
+    </div>
+</div>
+<!-- Page Header End -->
+
+
+<!-- Contact Start -->
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="row g-0">
+            <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+                <div class="bg-secondary p-5">
+                    <p class="d-inline-block bg-dark text-primary py-1 px-4">Contact Us</p>
+                    <h1 class="text-uppercase mb-4">Have Any Query? Please Contact Us!</h1>
+                    <p class="mb-4">The contact form is currently inactive. Get a functional and working contact form with Ajax & PHP in a few minutes. Just copy and paste the files, add a little code and you're done. <a href="https://htmlcodex.com/contact-form">Download Now</a>.</p>
+                    <form>
+                        <div class="row g-3">
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control bg-transparent" id="name" placeholder="Your Name">
+                                    <label for="name">Your Name</label>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="email" class="form-control bg-transparent" id="email" placeholder="Your Email">
+                                    <label for="email">Your Email</label>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control bg-transparent" id="subject" placeholder="Subject">
+                                    <label for="subject">Subject</label>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-floating">
+                                    <textarea class="form-control bg-transparent" placeholder="Leave a message here" id="message" style="height: 100px"></textarea>
+                                    <label for="message">Message</label>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <button class="btn btn-primary w-100 py-3" type="submit">Send Message</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
+                <div class="h-100" style="min-height: 400px;">
+                    <iframe class="google-map w-100 h-100"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"
+                    frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0" style="filter: grayscale(100%) invert(92%) contrast(83%); border: 0;"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Contact End -->
+
+@stop
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Jetstream\Rules\Role;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,12 +18,23 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::middleware('web')->group(function () {
+    Route::get('/index', function () {
+        return view('index');
+    })->name('olympus.index');
+    Route::get('/about', function () {
+        return view('about');
+    })->name('olympus.about');
+    Route::get('/service', function () {
+        return view('service');
+    })->name('olympus.service');
+    Route::get('/contact', function () {
+        return view('contact');
+    })->name('olympus.contact');
+    Route::group(['prefix' => 'pages'], function(){
+        Route::get('/price', function () {
+            return view('pages.price');
+        })->name('olympus.pages.price');
+    });
 });
+    

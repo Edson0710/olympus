343d094bb3e346f81d05b35e2721f9795ea31084
Edson Daniel Navarro Brizuela <edsonnavarro0710@gmail.com>
2022-11-06 12:31:46 -0600

    Update empleadosIndex.blade.php

--- resources/views/empleados/empleadosIndex.blade.php
+++ resources/views/empleados/empleadosIndex.blade.php
@@ -100,7 +100,27 @@
                         columns: [ 0, 1, 2, 3, 4, 5 ]
                     }
                 },
-            ]
+            ],
+            language: {
+                "decimal": "",
+                "emptyTable": "No hay información",
+                "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+                "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+                "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "Mostrar _MENU_ Entradas",
+                "loadingRecords": "Cargando...",
+                "processing": "Procesando...",
+                "search": "Buscar:",
+                "zeroRecords": "Sin resultados encontrados",
+                "paginate": {
+                    "first": "Primero",
+                    "last": "Ultimo",
+                    "next": "Siguiente",
+                    "previous": "Anterior"
+                }
+            }
         } );
     } );
 </script>
